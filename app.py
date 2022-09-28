c816791848f297e95757bb845cd7ad401e00d882
Dhanusha R <dhanusharamesh@gmail.com>
2022-09-28 15:16:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import imp
-from flask import Flask,render_template
+from flask import Flask,render_template         #Import flask module and render_template 
 
-app = Flask(__name__)
+app = Flask(__name__)                           #The variable __name__ is passed as first argument when creating an instance of the Flask object
 
-@app.route('/') 
+@app.route('/')                                 # Home page contents - "/ --> represents index page"
 @app.route('/home') 
 def home():
     print("---- Inside my Home page ------")
     return render_template('index.html')
 
-@app.route('/next')
+@app.route('/next')                            
 def nextpage():
     print("---- Next page ----")
     return render_template('next.html')
 
+#app.run(port=5000)                             #You can give any port number but it should be unique 
+
 if __name__ == '__main__' :
-    app.run(debug=True)
+    app.run(debug=True)                         #Flask starts the server listening on 127.0.0.1 and port 5000 by default
